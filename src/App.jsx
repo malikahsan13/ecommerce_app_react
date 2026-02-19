605f5c2f944a78b7e668ed380ef08512653d6f9e
Malik Ahsan <malikahsan13@gmail.com>
2026-02-20 00:02:31 +0500

    add routes, navbar and footer in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import ProductDetail from "./pages/ProductDetail";
+import Footer from "./components/Footer";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <div className="min-h-screen bg-gray-950 font-sans">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/Checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
+  );
 };
 
 export default App;
