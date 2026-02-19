d665941a6ddd185e608923ceae0592de8922829c
Malik Ahsan <malikahsan13@gmail.com>
2026-02-19 23:52:33 +0500

    created components and pages

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const App = () => {
+const CartItem = () => {
   return <div></div>;
 };
 
-export default App;
+export default CartItem;
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const App = () => {
+const Loading = () => {
   return <div></div>;
 };
 
-export default App;
+export default Loading;
