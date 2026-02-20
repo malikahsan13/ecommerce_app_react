6b1027cb10e29241bef78c46da120ed89be7282e
Malik Ahsan <malikahsan13@gmail.com>
2026-02-21 00:31:57 +0500

    added product card designs

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "lucide-react";
 
-const ProductCard = () => {
-  return <div></div>;
+const ProductCard = ({ product }) => {
+  console.log(product.image);
+  return (
+    <>
+      <div
+        className="bg-gray-900 rounded-2xl shadow-xl overflow-hidden flex flex-col h-full 
+      transition duration-500 transform border border-gray-800
+      hover:scale-[1.03] hover:shadow-orange-900/40"
+      >
+        <Link
+          to={"/product"}
+          className="relative cursor-pointer overflow-hidden"
+        >
+          <img
+            src={product.image}
+            alt={product.name}
+            className="w-full h-56 object-cover transition duration-500 group-hover:scale-110 group-hover:opacity-90"
+          />
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default ProductCard;
