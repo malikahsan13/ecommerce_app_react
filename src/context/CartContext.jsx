7c1485aa837108225483613397eba9084aa2e3e3
Malik Ahsan <malikahsan13@gmail.com>
2026-02-21 00:07:37 +0500

    created cart context and set context provider

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+
+const CartContext = createContext();
+import { initialProducts } from "../data/products";
+import React from "react";
+
+export const CartProvider = ({ props }) => {
+  const products = initialProducts;
+  return (
+    <CartContext.Provider value={{ products }}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
