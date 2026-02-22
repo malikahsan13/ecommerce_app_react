c0ee7d886a40129183e15870390992b91ef813bf
Malik Ahsan <malikahsan13@gmail.com>
2026-02-23 01:54:06 +0500

    started add to cart functionality

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,11 +1,15 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 import { initialProducts } from "../data/products";
 import React from "react";
 
 export const CartProvider = (props) => {
+  const [cart, setCart] = useState([]);
   const products = initialProducts;
+
+  const addToCart = (product) => {};
+
   return (
     <CartContext.Provider value={{ products }}>
       {props.children}
