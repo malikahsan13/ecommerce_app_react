9e6b9093ffdc8cfda1dcba6e18d9a552d7afd017
Malik Ahsan <malikahsan13@gmail.com>
2026-02-22 16:10:50 +0500

    complete product detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { initialProducts } from "../data/products";
-import { ChevronLeft, Tag } from "lucide-react";
+import { ChevronLeft, Tag, Zap, ShoppingCart } from "lucide-react";
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
@@ -46,10 +46,39 @@ const ProductDetail = () => {
             <p className="text-3xl font-extrabold text-orange-400 mb-4">
               Rs. {product.price.toFixed(2)}
             </p>
-            <h2>
-              <Tag />
-              <span></span>
+            <h2 className="text-xl font-bold text-gray-200 mb-2 border-b border-orange-900/50 pb-2 flex items-center space-x-2">
+              <Tag className="w-5 h-5 text-orange-500" />
+              <span>Product Overview</span>
             </h2>
+            <p className="text-gray=500 text-lg leading-relaxed mb-3">
+              {product.description}
+            </p>
+            <ul className="space-y-3 text-gray-300 p-4 bg-gray-800 rounded-xl border-gray-700">
+              <li className="flex items-center space-x-3 text-lg">
+                <Zap className="w-5-h-5 text-orange-500" />
+                <span>High Quality, Professional Grade Material</span>
+              </li>
+              <li className="flex items-center space-x-3 text-lg">
+                <Zap className="w-5-h-5 text-orange-500" />
+                <span>Comprehensive 1-Year Manufacturer Warrenty</span>
+              </li>
+              <li className="flex items-center space-x-3 text-lg">
+                <Zap className="w-5-h-5 text-orange-500" />
+                <span>Immediate Shipping for In-Stock Items</span>
+              </li>
+            </ul>
+
+            <div className="mt-5 space-y-5 flex justify-center items-center flex-col">
+              <button className="w-full py-3 bg-orange-600 rounded-full text-white font-bold rounnded-full shadow-lg shadow-orange-800/50 cursor-pointer hover:bg-orange-700 transition duration-300 flex items-center justify-center space-x-2 transform hover:ring-4 hover:ring-pint-600/50 tracking-wider">
+                <ShoppingCart className="w-6 h-6" /> ADD TO CART
+              </button>
+              <Link
+                to={"/"}
+                className="w-full py-3 border-2 cursor-pointer rounded-full border-orange-600 text-orange-400 hover:bg-orange-700 transition duration-30 flex items-center justify-center space-x-2 transform hober:ring-4 hover:ring-pink-600/50 uppercase tracking-wider hover:text-white"
+              >
+                Keep Shopping
+              </Link>
+            </div>
           </div>
         </div>
       </div>
