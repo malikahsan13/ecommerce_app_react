ede87bb529cd9dba3a47ef3269bbc0c0a3ece549
Malik Ahsan <malikahsan13@gmail.com>
2026-04-25 23:52:36 +0500

    added cart logic

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -8,7 +8,16 @@ export const CartProvider = (props) => {
   const [cart, setCart] = useState([]);
   const products = initialProducts;
 
-  const addToCart = (product) => {};
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find(item => item.id === product.id)
+      if(existingItem){
+        return prevCart.map((item) => item.id === product.id ? {...item, quantity:item.quantity + 1} : item)
+      } else {
+        return {...prevCart, {...product, quantity:1}}
+      }
+    })
+  };
 
   return (
     <CartContext.Provider value={{ products }}>
