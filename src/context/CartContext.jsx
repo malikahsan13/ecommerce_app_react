8bbc2e549784036dce19f0a266f82ed03b1e168e
Malik Ahsan <malikahsan13@gmail.com>
2026-04-25 23:53:06 +0500

    fixed bug in card logic

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -10,13 +10,17 @@ export const CartProvider = (props) => {
 
   const addToCart = (product) => {
     setCart((prevCart) => {
-      const existingItem = prevCart.find(item => item.id === product.id)
-      if(existingItem){
-        return prevCart.map((item) => item.id === product.id ? {...item, quantity:item.quantity + 1} : item)
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
       } else {
-        return {...prevCart, {...product, quantity:1}}
+        return [...prevCart, { ...product, quantity: 1 }];
       }
-    })
+    });
   };
 
   return (
