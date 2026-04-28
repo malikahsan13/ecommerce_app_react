2f2b1ec39242e7add1e1f1e36e9c3f9be013c5d2
Malik Ahsan <malikahsan13@gmail.com>
2026-04-28 23:22:09 +0500

    added back to store link

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import {useCart} from "../context/CartContext"
+import {Link} from "react-router-dom"
+import { ShoppingCart, ChevronLeft, Zap } from "lucide-react"
 
 const Cart = () => {
-  return <div></div>;
+  const { cart, cartTotol } = useCart()
+  return <>
+    <div className="container mx-auto px-4 md:px-8 pt-8">
+      <div className="flex items-center mb-10">
+        <Link to={"/"} className="flex items-center text-gray-400 hover:text-orange-400 transition-duration-150 font-semibold text-lg">
+          <ChevronLeft className="w-6 h-6 mr-1" />
+          <span>Back to Store</span>
+        </Link>
+      </div>
+    </div>
+  </>;
 };
 
 export default Cart;
