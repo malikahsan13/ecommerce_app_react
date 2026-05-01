4a84a437d791faa1c94a17c47ca1d2445fb56051
Malik Ahsan <malikahsan13@gmail.com>
2026-05-01 23:46:32 +0500

    added item name and price

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -10,6 +10,10 @@ const CartItem = ({item}) => {
   return <div className="flex flex-col items-center justify-between p-4 sm:p-6 mb-4 bg-gray-900 rounded-xl shadow-2xl border-gray-800 transition duration-300 hover:border-orange-600/50">
     <div className="flex items-center space-x-4 w-full sm:w-auto">
       <img src={item.image} alt={item.name} className="w-24 h-24 object-cover rounded-lg border-2 border-gray-700" />
+      <div>
+        <h3 className="text-xl font-bold text-white line-clamp-1">{item.name}</h3>
+      </div>
+      <p>{item.price.tofixed(2)}</p>
     </div>
 
   </div>;
