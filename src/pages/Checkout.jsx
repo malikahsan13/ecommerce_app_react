c73a70e92361089676a01dc9f35e8f511ec2f2ec
Malik Ahsan <malikahsan13@gmail.com>
2026-05-03 23:37:11 +0500

    added deliverydetails state

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useCart } from "../context/CartContext";
+import { Package, MagPin } from "lucide-react";
 
 const Checkout = () => {
-  return <div></div>;
+  const [deliverDetails, setDeliveryDetails] = useState({
+    name: "",
+    state: "",
+    city: "",
+    zip: "",
+  });
+
+  return (
+    <>
+      <div className="container mx-auto px-4 md:px-8 pt-8">
+        <h2 className="text-5xl font-extrabold text-white mb-10 tracking-tight">
+          Finalize Order{" "}
+        </h2>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10"></div>
+      </div>
+    </>
+  );
 };
 
 export default Checkout;
