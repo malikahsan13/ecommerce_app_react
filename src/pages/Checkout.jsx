297fa6517e692699b50c80084f7b6c07bc01ebab
Malik Ahsan <malikahsan13@gmail.com>
2026-05-03 23:40:27 +0500

    added heading in checkout page and did styling

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,7 +3,7 @@ import { useCart } from "../context/CartContext";
 import { Package, MagPin } from "lucide-react";
 
 const Checkout = () => {
-  const [deliverDetails, setDeliveryDetails] = useState({
+  const [deliveryDetails, setDeliveryDetails] = useState({
     name: "",
     state: "",
     city: "",
@@ -16,7 +16,14 @@ const Checkout = () => {
         <h2 className="text-5xl font-extrabold text-white mb-10 tracking-tight">
           Finalize Order{" "}
         </h2>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10"></div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+          <div className="lg:col=span-2 p-8 bg-gray-900 rounded-2xl shadow-2xl border border-gray-800">
+            <h3 className="text-3xl font-bold text-orange-400 mb-6 flex items-center cpsace-x-3 border-b border-bray-700 pb-4">
+              <MagPin className="w-7 h-7 text-orange-500" />
+              <span>Shopping Information</span>
+            </h3>
+          </div>
+        </div>
       </div>
     </>
   );
