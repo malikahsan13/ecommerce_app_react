75539c8991a9ed1173c3156b5dd94135cad4d4a7
Malik Ahsan <malikahsan13@gmail.com>
2026-05-07 23:50:35 +0500

    added props in search filter and updated input control attrib

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -27,7 +27,7 @@ const ProductList = () => {
           Featured Gear ({products.length} Items)
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-center items-center">
-          {products.map((product, index) => (
+          {filterProducts.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
         </div>
